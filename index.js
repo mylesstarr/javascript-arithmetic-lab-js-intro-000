66278d3e939b0cce2e5aae41dc37b1a17470a547
Myles Starr <mylesstarr@gmail.com>
2021-03-23 13:30:49 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(x,y) {
+return x + y
+}
+
+function subtract(x,y) {
+return x - y
+}
+
+function multiply(x,y) {
+return x * y
+}
+
+function divide(x,y) {
+return x / y
+}
+
+function increment(n) {
+var number = n
+return n += 1
+}
+
+function decrement(n) {
+return n -= 1
+}
